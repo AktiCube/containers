7c9a75e690904ad4803d36f04fcea89eb6ae19c0
Louis <louis.rds@akticube.fr>
2022-07-20 16:46:09 +0200

    Remove print of actual command for OuiPanel Image

--- Pterodactyl/GraalVM-OH/entrypoint.sh
+++ Pterodactyl/GraalVM-OH/entrypoint.sh
@@ -41,9 +41,5 @@ java -version
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-
-# Display the command we're running in the output, and then execute it with the env
-# from the container itself.
-printf "$PARSED"
 # shellcheck disable=SC2086
 exec env ${PARSED}
