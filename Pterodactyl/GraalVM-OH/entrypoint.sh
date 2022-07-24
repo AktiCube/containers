1df7cd0d90fc3bedc92253242ab6880e4817ac71
Louis <louis.rds@akticube.fr>
2022-07-24 19:33:59 +0200

    Remove print java -version

--- Pterodactyl/GraalVM-OH/entrypoint.sh
+++ Pterodactyl/GraalVM-OH/entrypoint.sh
@@ -33,10 +33,6 @@ export INTERNAL_IP
 # Switch to the container's working directory
 cd /home/container || exit 1
 
-# Print Java version
-printf "java -version\n"
-java -version
-
 # Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
 # variable format of "${VARIABLE}" before evaluating the string and automatically
 # replacing the values.
